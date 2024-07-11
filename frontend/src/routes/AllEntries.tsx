792bf2d9fc5877eb07ce831af01b38167635f2c9
Adam Howard <adam101howard@gmail.com>
2024-07-11 13:09:26 +0100

    update list items with theme

--- frontend/src/routes/AllEntries.tsx
+++ frontend/src/routes/AllEntries.tsx
@@ -29,10 +29,13 @@ export default function AllEntries() {
           <div
             id={entry.id}
             key={index}
-            className={colourTheme['list-item-background'] + " shadow-md shadow-gray-500 m-3 p-4 rounded flex flex-col justify-between"}
+            className={
+              colourTheme["list-item-background"] +
+              " shadow-md shadow-gray-500 m-3 p-4 rounded flex flex-col justify-between"
+            }
           >
-            <h1 className="font-bold text-sm md:text-lg">{entry.title}</h1>
-            <p className="text-center text-lg font-light md:mt-2 md:mb-4 mt-1 mb-3">{entry.description}</p>
+            <h1 className={colourTheme["list-item-text"] + " font-bold text-sm md:text-lg"}>{entry.title}</h1>
+            <p className={colourTheme["list-item-text"] + " text-center text-lg font-light md:mt-2 md:mb-4 mt-1 mb-3"}>{entry.description}</p>
             <section className="flex items-center justify-between flex-col md:flex-row pt-2 md:pt-0">
               <div className="flex justify-center">
                 <button
@@ -52,7 +55,7 @@ export default function AllEntries() {
                   🖊
                 </button>
               </div>
-              <time className="text-right text-sm md:text-lg">
+              <time className={colourTheme["list-item-text"] + " text-right text-sm md:text-lg"}>
                 {new Date(entry.created_at.toString()).toLocaleDateString()}
               </time>
             </section>
