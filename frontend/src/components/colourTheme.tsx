52893eac819822ac9d5a2f6f0a31bba45c13ed1a
Adam Howard <adam101howard@gmail.com>
2024-07-11 13:17:03 +0100

    add scheduled column

--- frontend/src/components/colourTheme.tsx
+++ frontend/src/components/colourTheme.tsx
@@ -1,8 +1,8 @@
 // https://stackoverflow.com/questions/34351804/how-to-declare-a-global-variable-in-react
 
 const colourTheme = {
-    "theme" : 'light',
-    "list-item-background" : 'bg-gray-300',
-    'list-item-text' : 'text-gray-950'
+  theme: "light",
+  "list-item-background": "bg-gray-300",
+  "list-item-text": "text-gray-950",
 };
 export default colourTheme;
